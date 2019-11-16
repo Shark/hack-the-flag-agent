3b0ca627d140e22559285e74b7910ff35dcc3289
Jonas Schiefner <jonas.schiefner@zweitag.de>
2019-11-16 14:42:57 +0200

    update file_importer with new json parse fixes

--- file_importer.rb
+++ file_importer.rb
@@ -15,7 +15,8 @@ processed_event_ids = {}
 def fetch_live_feed
   movements = []
   resp = HTTP.timeout(1).get(ENV.fetch('LIVE_FEED_URL'))
-  body = JSON.parse(resp.body)
+  # body = JSON.parse(resp.body)
+  body = parse_live_feed(resp.body)
   body.each do |el|
     el['notifications'].each do |m|
       unless m['notificationType'] == 'locationupdate'
@@ -24,7 +25,7 @@ def fetch_live_feed
       end
 
       unless m['hierarchyDetails']['building']['name'] == 'Väre'
-        puts "skipping because builing #{m['hierarchyDetails']['building']['name']}"
+        puts "skipping because building #{m['hierarchyDetails']['building']['name']}"
         next
       end
 
@@ -40,8 +41,18 @@ def fetch_live_feed
     end
   end
   movements
-rescue => e
-  puts "Error fetching live feed: #{e}"
+rescue HTTP::ConnectionError => e
+  puts "Error fetch live feed: #{e}"
+  []
+end
+
+def parse_live_feed(body)
+  str = "[#{body.to_s.delete_suffix(",\n")}]"
+  str.gsub!('} {', '},{')
+  str.gsub!('}{', '},{')
+  JSON.parse(str)
+rescue JSON::ParserError => e
+  puts "Error fetch live feed: #{e}"
   []
 end
 
@@ -49,16 +60,22 @@ loop do
   points = fetch_live_feed
            .select {|m| processed_event_ids[m.event_id] == nil }
            .map do |m|
+            floor_mapping = {
+              0 => 'zero',
+              1 => 'one',
+              2 => 'two',
+              3 => 'three',
+            }
             puts m
              processed_event_ids[m.event_id] = true
              {
               series: 'movement',
               values: {x: m.x, y: m.y, z: m.z},
-              tags: {mac: m.mac, ssid: m.ssid},
+              tags: {mac: m.mac, ssid: m.ssid, floor: floor_mapping.fetch(m.z, 'NOT APPLICABLE')},
               timestamp: m.timestamp.to_i,
             }
            end
   influxdb.write_points(points)
   puts "Imported #{points.count} movements"
-  sleep 0.1
+  sleep 1
 end
